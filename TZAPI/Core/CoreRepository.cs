fdecf2790c9dd20ce7f772cc68f4bc0db6b2f6fc
T0dS1K <sampalov167@gmail.com>
2025-09-22 12:19:35 +0300

    Добавлен метод считывания файла, а также заполнение словаря

--- TZAPI/Core/CoreRepository.cs
+++ TZAPI/Core/CoreRepository.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
 
 namespace TZAPI.Core
 {
@@ -7,20 +9,21 @@ namespace TZAPI.Core
         private readonly object LockUpdate = new();
         private ConcurrentDictionary<string, List<string>> DataStorage = new();
 
-        public CoreRepository() { }
+        public CoreRepository()
+        {
+            UpdateData(out string em);
+            Console.WriteLine(em);
+        }
 
         public bool UpdateData(out string em)
         {
             try
             {
                 var StringLines = File.ReadAllLines("AdvertisingPlatforms.txt");
-                ConcurrentDictionary<string, List<string>> NewDataStorage = new();
 
                 lock (LockUpdate)
                 {
-                    /////////////////////////////
-                    
-                    DataStorage = NewDataStorage;
+                    DataStorage = ReadingData(StringLines);
                 }
 
                 em = "Ok";
@@ -48,6 +51,57 @@ namespace TZAPI.Core
             }
 
             return AdvertisingPlatforms;
+        }
+
+        private ConcurrentDictionary<string, List<string>> ReadingData(string[] StringLines)
+        {
+            var sb = new StringBuilder();
+            ConcurrentDictionary<string, List<string>> NewDataStorage = new();
+
+            foreach (var Line in StringLines)
+            {
+                if (string.IsNullOrWhiteSpace(Line)) continue;
+                var Split = Line.Split(':');
+                if (Split.Length != 2) continue;
+
+                var Platform = Split[0].Trim();
+                var KeysList = Split[1].Trim().Split(',').Select(p => p.Trim()).ToList();
+
+                foreach (var Key in KeysList)
+                {
+                    NewDataStorage.AddOrUpdate(
+                        Key,
+                        Z => new List<string> { Platform },
+                        (Z, DataStorage) =>
+                        {
+                            if (!DataStorage.Contains(Platform))
+                            {
+                                DataStorage.Add(Platform);
+                            }
+                            return DataStorage;
+                        });
+                }
+            }
+
+            foreach (var Key in NewDataStorage.Keys)
+            {
+                sb.Clear();
+                var Split = Key.Split('/', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < Split.Length - 1; i++)
+                {
+                    sb.Append('/');
+                    sb.Append(Split[i]);
+                    string path = sb.ToString();
+
+                    if (NewDataStorage.TryGetValue(path, out var DataStorage))
+                    {
+                        NewDataStorage[Key] = NewDataStorage[Key].Union(DataStorage).ToList();
+                    }
+                }
+            }
+
+            return NewDataStorage;
         }
     }
 }
